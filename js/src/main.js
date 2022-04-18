68a47836c56aae9e7659b18f9474b6ab2fba9737
kyoto-01 <cameraman269@gmail.com>
2022-04-17 21:47:21 -0300

    feat: delete host feature added

--- js/src/main.js
+++ js/src/main.js
@@ -2,5 +2,5 @@ import { hosts } from './data/hosts.js';
 import { load_hosts, add_host } from './lib/host_cards.js';
 
 
-load_hosts(hosts, "#card-list .container .row");
+load_hosts(hosts, "#card-list .container");
 add_host();
