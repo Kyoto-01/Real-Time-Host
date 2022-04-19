eed60d3bef34dc798c86e48716e967af0c54c465
kyoto-01 <cameraman269@gmail.com>
2022-04-19 20:32:15 -0300

    chore: added node_modules to .gitignore

--- js/src/main.js
+++ js/src/main.js
@@ -1,6 +1,12 @@
-import { hosts } from './data/hosts.js';
 import { load_hosts, add_host } from './lib/host_cards.js';
 
 
-load_hosts(hosts, "#card-list .container");
+async function show_hosts(parentSelector){
+    const response = await fetch('http://127.0.0.1:3000/hosts');
+    const hostList = await response.json();
+    console.log(hostList);
+    load_hosts(hostList, parentSelector)
+}
+
+show_hosts("#card-list .container");
 add_host();
