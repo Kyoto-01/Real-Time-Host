6b57fc7dfe51f1630f95b445b4abaaeaf583a3d6
kyoto-01 <cameraman269@gmail.com>
2022-04-20 14:01:45 -0300

    feat(host_requests.js): Create and delete in the json server

--- js/src/main.js
+++ js/src/main.js
@@ -1,12 +1,4 @@
-import { load_hosts, add_host } from './lib/host_cards.js';
+import { load_hosts } from './lib/host_cards.js';
 
 
-async function show_hosts(parentSelector){
-    const response = await fetch('http://127.0.0.1:3000/hosts');
-    const hostList = await response.json();
-    console.log(hostList);
-    load_hosts(hostList, parentSelector)
-}
-
-show_hosts("#card-list .container");
-add_host();
+load_hosts('#card-list .container');
