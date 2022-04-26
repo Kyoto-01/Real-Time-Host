225681c75e579e21128449c1794c385f8f67c246
kyoto-01 <cameraman269@gmail.com>
2022-04-25 22:30:10 -0300

    refactor(general): modularize, rearrange directory structure, most readable code, etc...

--- front/js/lib/host_cards.js
+++ front/js/lib/host_cards.js
@@ -1,33 +1,31 @@
-import api from "../services/api.js";
-import { show_host_details } from "./host_details.js";
+import hosts from './hosts.js'
+import hostDetails from "./host_details.js";
 
 
 /*
     Funções e eventos relacionados á exibição, adição e exclsão de cards de hosts
 */
 
-const hostsResource = '/hosts';
+async function load_hosts(parentSelector) {
+    const hostList = await hosts.get_hosts();
 
-add_host();
-
-async function load_hosts(parentSelector){
-    const hostList = await api.read(hostsResource);
+    hosts.host_events();
     create_host_card_group(hostList, parentSelector);
 }
 
-function create_host_card_group(hostList, parentSelector){
+function create_host_card_group(hostList, parentSelector) {
     const hostGroupHTML = '<div id="host-group" class="row row-cols-1 row-cols-md-2 row-cols-xl-3"></div>';
 
     let parent = document.querySelector(parentSelector);
     parent.innerHTML = hostGroupHTML;
 
     parent = parent.querySelector('#host-group');
-    for (let host of hostList){
+    for (let host of hostList) {
         create_host_card(parent, host);
     }
 }
 
-function create_host_card(parent, hostData){
+function create_host_card(parent, hostData) {
     const hostCardID = `host-${hostData.id}`;
     const hostCardHTML = `
     <div class="col mt-4">
@@ -35,7 +33,7 @@ function create_host_card(parent, hostData){
             <div class="card-header">
                 <img src="images/pc.svg" alt="PC" class="card-img-top card-icon"> 
                 <span class="float-end">
-                    <i class="fa-solid fa-circle ${hostData.general.online ? 'fa-circle-on' : 'fa-circle-off' }"></i> ${hostData.general.online ? 'online' : 'offline'}
+                    <i class="fa-solid fa-circle ${hostData.general.online ? 'fa-circle-on' : 'fa-circle-off'}"></i> ${hostData.general.online ? 'online' : 'offline'}
                 </span>
             </div>
             <div class="card-body">
@@ -59,59 +57,13 @@ function create_host_card(parent, hostData){
             </div>
         </div>
     </div>`;
-    
+
     parent.insertAdjacentHTML('beforeend', hostCardHTML);
 
-    document.querySelector(`#${hostCardID} .btn-primary`).onclick = () => show_host_details(hostData);
-    document.querySelector(`#${hostCardID} .btn-outline-danger`).onclick = () => del_host(hostData);
+    document.querySelector(`#${hostCardID} .btn-primary`).onclick = () => hostDetails.show_host_details(hostData);
+    document.querySelector(`#${hostCardID} .btn-outline-danger`).onclick = () => hosts.del_host(hostData);
 }
 
-function add_host(){
-    const addHostModal = new bootstrap.Modal(document.getElementById('add-host-modal'));
-    const form = document.getElementById('add-host-form');
 
-    document.getElementById('add-host-btn').onclick = () => addHostModal.show();
 
-    form.onsubmit = (event) => {
-
-        event.preventDefault();
-
-        const id = new Date().getTime();
-        const hostname = form.querySelector('#add-host-name').value;
-        const ip = form.querySelector('#add-host-ip').value;
-        const os = form.querySelector('#add-host-so').value;
-
-        const newHost = {
-            id: id,
-            general: {
-                hostname: hostname,
-                ip: ip,
-                os: os,
-                online: true,
-            },
-        };
-
-        api.create(hostsResource, newHost);
-
-        form.reset();
-        addHostModal.hide();
-    };
-}
-
-function del_host(hostData){
-    const delHostModalElement = document.getElementById('del-host-confirm-modal');
-    const delHostModalBody = delHostModalElement.querySelector('.modal-body');
-    const delHostConfirmButton = delHostModalElement.querySelector('.btn-danger');
-    const delHostModal = new bootstrap.Modal(delHostModalElement);   
-
-    delHostModalBody.innerHTML = `Deseja remover o host ${hostData.general.hostname} (${hostData.general.ip})?`;
-
-    delHostConfirmButton.onclick = function() {
-        api.destroy(`${hostsResource}/${hostData.id}`);
-    };
-
-    delHostModal.show();
-}
-
-export { load_hosts };
- 
+export default { load_hosts };
