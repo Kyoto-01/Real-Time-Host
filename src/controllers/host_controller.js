6535f835014bb75492d573b074c7a7bd8f3d7f95
kyoto-01 <cameraman269@gmail.com>
2022-06-21 23:12:45 -0300

    refactor: modularize host route functions in a host controller

---
+++ src/controllers/host_controller.js
@@ -0,0 +1,55 @@
+import hostModel from '../models/host.js';
+import hostManager from '../manager/host_manager.js';
+
+
+function index(req, res) {
+    const cached = Boolean(req.query.cached);
+    let response = hostModel.read_all();
+
+    if (!cached) {
+        response = hostManager.get_hosts(response);
+    }
+
+    res.json(response);
+}
+
+function read_by_id(req, res) {
+    const id = req.params.id;
+    let response = hostModel.read_by_id(id);
+    
+    response = hostManager.get_host(response);
+
+    res.json(response);
+}
+
+function create(req, res) {
+    const data = req.body;
+    const response = hostModel.create(data);
+
+    res.status(201).json(response);
+}
+
+function update(req, res) {
+    const id = req.query.id;
+    const data = req.body;
+    const response = hostModel.update(id, data);
+
+    res.json(response);
+}
+
+function destroy(req, res) {
+    const id = req.query.id;
+
+    hostModel.remove(id);
+    
+    res.status(204).send();
+}
+
+
+export default {
+    index,
+    read_by_id,
+    create,
+    update,
+    destroy
+}
--- src/routes/index.js
+++ src/routes/index.js
@@ -2,54 +2,22 @@ import express from 'express';
 import bcrypt from 'bcrypt';
 import jwt from 'jsonwebtoken';
 
-import hostsGeneralManager from '../manager/hosts_general.js';
-import hostAllManager from '../manager/host_all.js';
-import hostsModel from '../models/host.js';
-import userModel from '../models/user.js';
+import hostController from '../controllers/host_controller.js';
 import auth from '../middlewares/auth.js';
 
 
 const router = express.Router();
 
 
-router.get('/hosts', auth.is_authenticated, (req, res) => {
-    const cached = Boolean(req.query.cached);
-    let hostList = hostsModel.read();
+router.get('/hosts', auth.is_authenticated, hostController.index);
 
-    if (!cached) {
-        hostList = hostsGeneralManager.get_hosts_general(hostList);
-    }
+router.post('/hosts', auth.is_authenticated, hostController.create);
 
-    res.json(hostList);
-});
+router.put('/hosts', auth.is_authenticated, hostController.update);
 
-router.post('/hosts', auth.is_authenticated, (req, res) => {
-    const newHost = req.body;
-    const created = hostsModel.create(newHost);
-    res.status(201).json(created);
-});
+router.delete('/hosts', auth.is_authenticated, hostController.destroy);
 
-router.put('/hosts', auth.is_authenticated, (req, res) => {
-    const hostID = req.query.id;
-    const newData = req.body;
-    const updated = hostsModel.update(hostID, newData);
-    res.json(updated);
-});
-
-router.delete('/hosts', auth.is_authenticated, (req, res) => {
-    const hostID = req.query.id;
-    hostsModel.remove(hostID);
-    res.status(204).send();
-});
-
-router.get('/hosts/:id', auth.is_authenticated, async (req, res) => {
-    const id = req.params.id;
-    const hostList = hostsModel.read();
-    const host = hostList.find(value => value.id == id);
-    const hostInfos = await manager.get_host_all(host);
-
-    res.json(hostInfos);
-});
+router.get('/hosts/:id', auth.is_authenticated, hostController.read_by_id);
 
 router.post('/users', async (req, res) => {
     const user = req.body;
